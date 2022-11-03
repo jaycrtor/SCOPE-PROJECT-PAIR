ab029ba20eb8a69a0c169b1d3abfb26f40715387
NygilNet <nygilnet@gmail.com>
2022-11-03 14:57:38 -0500

    first two problems

--- problems/01-arrow-full-name.js
+++ problems/01-arrow-full-name.js
@@ -16,6 +16,8 @@ arrowGetFullName(p2); // => 'Charlie Brown'
 
 // Your code here
 
+let arrowGetFullName = person => {return person.firstName + ' ' + person.lastName};
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
